b5e38fc245c8551dc7d9f26638a3705746bb366f
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-10-31 09:40:10 +0700

    == berguna untuk membedakan mana laki laki dan perempuan

--- app/src/main/java/com/example/prak5/TeksEdit.kt
+++ app/src/main/java/com/example/prak5/TeksEdit.kt
@@ -1,8 +1,13 @@
 package com.example.prak5
 
+import android.inputmethodservice.Keyboard.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import java.lang.reflect.Modifier
 
 @Composable
@@ -23,5 +28,30 @@ fun FromDataDiri(modifier: Modifier)
     val gender:List<String> = listOf("Laki-Laki", "Perempuan")
 
     Column(modifier= Modifier.padding(top= 50.dp)
-    verticalArrangement)
+    verticalArrangement = Arrangement.Top,
+
+        horizontalAligment = Alignment.Top,
+        OutlinedTextField(
+            value = textName,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+
+            label = { Text(text = "Nama Lengkap") },
+
+            onValueChange = {
+                textName = it
+            }
+        )
+
+        Row{
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+
+                    onClick = {textJK = item}
+                ))
+            }
+        }
+    )
 })

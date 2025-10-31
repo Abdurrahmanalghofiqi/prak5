1e3af05628c8121c9658834b2577aa61f9bf22c5
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-10-31 09:24:49 +0700

    pp

--- app/src/main/java/com/example/prak5/TeksEdit.kt
+++ app/src/main/java/com/example/prak5/TeksEdit.kt
@@ -21,4 +21,7 @@ fun FromDataDiri(modifier: Modifier)
     var jenis by remember { mutableStateOf( value: "") }
 
     val gender:List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier= Modifier.padding(top= 50.dp)
+    verticalArrangement)
 })
